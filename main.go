eb51fda4ae703656b763ae4f445e7ece5e1cb29a
kinako555 <dodohunter_3658@yahoo.co.jp>
2021-01-06 08:40:17 +0000

    mod router

--- main.go
+++ main.go
@@ -6,20 +6,15 @@ import (
 	"net/http"
 
 	"gin_tutorial/pkg/setting"
-	"github.com/gin-gonic/gin"
+	"gin_tutorial/routers"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
 	config, err := setting.NewConfig()
 	if err != nil {
 		log.Fatal("Can init Config with Enviroment:", err)
 	}
+	router := routers.InitRouter(config.AppConfig)
 	s := &http.Server{
 		Addr: fmt.Sprintf(":%d", config.HTTPPort),
 		Handler: router,
